e71a493ea2eb7ab21af6fbfdde0d1b960f3176f8
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-22 15:39:55 -0300

    Changed name

--- logistic-regression-neural-network/TrainLogisticRegression.py
+++ logistic-regression-neural-network/TrainLogisticRegression.py
@@ -1,5 +1,8 @@
 import numpy as np
 import mnist_reader
+import time
+
+from LogisticRegression import LogisticRegression
 
 # labels = {0: "T-shirt/top", 
 # 			1: "Trouser", 
@@ -13,14 +16,10 @@ import mnist_reader
 # 			9: "Ankle boot"}
 labels = {0: "T-shirt/top"}
 
-from LogisticRegression import LogisticRegression
-
 if __name__ == "__main__":
 	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
 	x_original_validation, y_original_validation = x_original_train[-10000:], y_original_train[-10000:]
 	x_original_train, y_original_train = x_original_train[:-10000], y_original_train[:-10000]
-
-	x_original_test, y_original_test = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='t10k')
 
 	"""
 		Suponha que queremos predizer se eh uma label l ou nao. Entao devemos
@@ -28,9 +27,14 @@ if __name__ == "__main__":
 		2) Trocar todas as labels != l por 0
 	"""
 	for label in labels:
+		start_time = time.time()
+
 		y_train = y_original_train.copy()
 		y_train[y_original_train == label] = 1
 		y_train[y_original_train != label] = 0
 
 		lr = LogisticRegression()
 		lr.fit(x_original_train, y_train)
+		lr.predict(x_original_validation, y_original_validation)
+
+		print("Elapsed time: %1f s" %(elapsed_time))

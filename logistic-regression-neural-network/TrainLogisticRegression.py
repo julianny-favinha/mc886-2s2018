1ea5efd6facae657acc1bd416193bd8e0d52e4f8
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-22 20:12:48 -0300

    Fixed pred

--- logistic-regression-neural-network/TrainLogisticRegression.py
+++ logistic-regression-neural-network/TrainLogisticRegression.py
@@ -4,7 +4,7 @@ import time
 
 from LogisticRegression import LogisticRegression
 
-"""labels = {0: "T-shirt/top", 
+labels = {0: "T-shirt/top", 
  			1: "Trouser", 
  			2: "Pullover", 
  			3: "Dress", 
@@ -13,19 +13,12 @@ from LogisticRegression import LogisticRegression
  			6: "Shirt", 
  			7: "Sneaker", 
  			8: "Bag",
- 			9: "Ankle boot"}"""
-
-labels = {0: "T-shirt/top", 
- 			1: "Trouser"}
+ 			9: "Ankle boot"}
 
 if __name__ == "__main__":
 	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
 	x_original_validation, y_original_validation = x_original_train[-10000:], y_original_train[-10000:]
 	x_original_train, y_original_train = x_original_train[:-10000], y_original_train[:-10000]
-
-	x_original_validation = x_original_validation[-10:]
-	y_original_validation = y_original_validation[-10:]
-	print("Original validation ", x_original_validation.shape)
 
 	"""
 		Suponha que queremos predizer se eh uma label l ou nao. Entao devemos
@@ -42,25 +35,24 @@ if __name__ == "__main__":
 
 		lr = LogisticRegression()
 		lr.fit(x_original_train, y_train)
-		predictions = np.append(pred, lr.predict(x_original_validation))
+
+		predictions = np.append(predictions, lr.predict(x_original_validation))
 
 		elapsed_time = time.time() - start_time
 		print("Elapsed time: %1f s" %(elapsed_time))
 
-	predictions = predictions.reshape(len(labels.keys()), x_original_validation.shape[1])
-	print(predictions.shape)
+	predictions = predictions.reshape(len(labels.keys()), x_original_validation.shape[0])
+	print("predictions", predictions)
+	print("predictions shape", predictions.shape)
 
 	predicted_class = []
-	for i in range(shape.predictions[1]):
+	for i in range(predictions.shape[1]):
 		column = predictions[:,i]
-		predicted_class.append(index(max(column)))
-	print(predicted_class)
+		predicted_class.append(np.argmax(column))
+	print("predicted_class", predicted_class)
 	
 	correct = 0
-	for i in range(predicted_class):
+	for i in range(len(predicted_class)):
 		if predicted_class[i] == y_original_validation[i]:
 			correct += 1
-	print(correct)
-
-	for i in range(500):
-		print("\a")
+	print("correct", correct)

0baacbfb9a0d5b38546baccfb09764f682e145ab
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-22 15:40:13 -0300

    Fixed bugs

--- logistic-regression-neural-network/LogisticRegression.py
+++ logistic-regression-neural-network/LogisticRegression.py
@@ -11,7 +11,7 @@ class LogisticRegression:
 		print("Performing fit...")
 
 		initialGuess = np.ones(x.shape[1]) # MELHORAR: quais valores de theta comecar?
-		learningRate = 0.00001 # MELHORAR: qual valor?
+		learningRate = 0.0000001 # MELHORAR: qual valor?
 
 		self.coefficients = descent(initialGuess, self.coefficients, x, y, learningRate)
 		print(self.coefficients)
@@ -19,6 +19,19 @@ class LogisticRegression:
 	"""
 		Uses model (coefficients) to predict y given x
 	"""
-	def predict(x):
+	def predict(self, xs, ys):
+		correct = 0
+
 		print("Performing predictions...")
-		return 1 / (1 + np.exp(-np.sum(coefficients * x)))
+		for i in range(xs.shape[0]):
+			prediction = 1 / (1 + np.exp(-np.sum(self.coefficients * xs[i])))
+			if prediction >= 0.5:
+				prediction = 1
+			else:
+				prediction = 0
+
+			if prediction == ys[i]:
+				correct += 1
+
+		print("Number of correct predictions:", correct)
+

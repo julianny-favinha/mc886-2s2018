cc7aa67a0ebbda73aeb62696c6eaa682ffba9cf9
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-22 20:40:40 -0300

    Predicting. But using logistic regression

--- logistic-regression-neural-network/MultinomialLogisticRegression.py
+++ logistic-regression-neural-network/MultinomialLogisticRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 from GradientDescent import descent
 
 class MultinominalLogisticRegression:
-	coefficients = None # MELHORAR
+	coefficients = None
 
 	"""
 		Find coefficients
@@ -10,19 +10,22 @@ class MultinominalLogisticRegression:
 	def fit(self, x, y, options={}):
 		print("Performing fit...")
 
-		# initialGuess = np.ones(x.shape[1]) # MELHORAR: quais valores de theta comecar?
-		# learningRate = 0.0000001 # MELHORAR: qual valor?
+		initialGuess = np.ones(x.shape[1]) # MELHORAR: quais valores de theta comecar?
+		learningRate = 0.0000001 # MELHORAR: qual valor?
 
-		# self.coefficients = descent(initialGuess, self.coefficients, x, y, learningRate)
+		self.coefficients = descent(initialGuess, self.coefficients, x, y, learningRate)
 
 	"""
 		Uses model (coefficients) to predict y given x
-	"""
-	def predict(self, xs):
-		print("Performing predictions...")
+	# """
+	# def predict(self, xs):
+	# 	print("Performing predictions...")
 
-		# predictions = []
-		# for i in range(xs.shape[0]):
-		# 	predictions.append(1 / (1 + np.exp(-np.sum(self.coefficients * xs[i]))))
+	# 	predictions = []
+	# 	for i in range(xs.shape[0]):
+	# 		predictions.append(1 / (1 + np.exp(-np.sum(self.coefficients * xs[i]))))
 			
-		# return predictions
+	# 	return predictions
+
+	def score(self, x):
+		return np.sum(self.coefficients * x)
--- logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
+++ logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 import mnist_reader
 import time
 
-from LogisticRegression import LogisticRegression
+from MultinominalLogisticRegression import MultinominalLogisticRegression
 
 """labels = {0: "T-shirt/top", 
  			1: "Trouser", 
@@ -22,3 +22,27 @@ if __name__ == "__main__":
 	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
 	x_original_validation, y_original_validation = x_original_train[-10000:], y_original_train[-10000:]
 	x_original_train, y_original_train = x_original_train[:-10000], y_original_train[:-10000]
+
+	mlr = MultinominalLogisticRegression()
+
+	coefficients = np.array([])
+	for label in labels:
+		y_train = y_original_train.copy()
+		y_train[y_original_train == label] = 1
+		y_train[y_original_train != label] = 0
+
+		mlr.fit(x_original_train, y_train)
+
+		coefficients = np.append(coefficients, mlr.coefficients)
+	coefficients = coefficients.reshape(len(labels.keys()), x_original_validation.shape[0])
+
+	scores = np.array([])
+	for coef in coefficients:
+		scores.append(np.sum(coef * x_original_validation))
+
+	predictions = np.array([])
+	sum_exponential = np.sum(np.exp(scores))
+	for score in scores:
+		predictions.append(np.exp(score) / sum_exponential)
+
+	

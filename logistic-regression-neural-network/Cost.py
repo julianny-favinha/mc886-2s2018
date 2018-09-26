ba264b43c20a7d51c826d098fe0f2a989f4a97b2
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-26 15:14:04 -0300

    Only operating with arrays, removed loop

--- logistic-regression-neural-network/Cost.py
+++ logistic-regression-neural-network/Cost.py
@@ -1,13 +1,9 @@
 import numpy as np
 
-def hypothesis(thetas, xs):
-	return 1 / (1 + np.exp(-np.sum(thetas * xs)))
+def hyp(coefficients, xs):
+	return 1 / (1 + np.exp(-np.dot(coefficients, xs.T)))
 
-def cost(thetas, xs, ys):
-	m = len(xs)
-	sum = 0
-
-	for i in range(m):
-		sum += ys[i]*np.log(hypothesis(thetas, xs[i])) + (1 - ys[i])*np.log(1 - hypothesis(thetas, xs[i]))
+def cost(thetas, xs, ys):	
+	sum = np.sum(ys*np.log(hyp(thetas, xs) + 0.00000001) + (1 - ys)*np.log(1 - hyp(thetas, xs) + 0.00000001))
 	
-	return -(1/m) * sum
+	return -(1/len(xs)) * sum

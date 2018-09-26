6199b00aa81ddd2f4a1f4a4db282b40f5128aa1b
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-26 15:19:37 -0300

    Created functions, descent using linear not sigmoid

--- logistic-regression-neural-network/MultinomialLogisticRegression.py
+++ logistic-regression-neural-network/MultinomialLogisticRegression.py
@@ -1,31 +1,23 @@
 import numpy as np
+
 from GradientDescent import descent
 
-class MultinominalLogisticRegression:
+class MultinomialLogisticRegression:
 	coefficients = None
+
+	@staticmethod
+	def linear(coefficients, xs):
+		return np.sum(coefficients * xs, axis=1)
+
+	model = linear
 
 	"""
 		Find coefficients
 	"""
-	def fit(self, x, y, options={}):
-		print("Performing fit...")
+	def fit(self, x, y, label, initialGuess, learningRate, iterations):
+		print("Performing fit for label {}...".format(label))
 
-		initialGuess = np.ones(x.shape[1]) # MELHORAR: quais valores de theta comecar?
-		learningRate = 0.0000001 # MELHORAR: qual valor?
-
-		self.coefficients = descent(initialGuess, self.coefficients, x, y, learningRate)
-
-	"""
-		Uses model (coefficients) to predict y given x
-	# """
-	# def predict(self, xs):
-	# 	print("Performing predictions...")
-
-	# 	predictions = []
-	# 	for i in range(xs.shape[0]):
-	# 		predictions.append(1 / (1 + np.exp(-np.sum(self.coefficients * xs[i]))))
-			
-	# 	return predictions
+		self.coefficients = descent(initialGuess, self.model, x, y, learningRate, iterations)
 
 	def score(self, x):
 		return np.sum(self.coefficients * x)
--- logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
+++ logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 import mnist_reader
 import time
 
-from MultinominalLogisticRegression import MultinominalLogisticRegression
+from MultinomialLogisticRegression import MultinomialLogisticRegression
 
 """labels = {0: "T-shirt/top", 
  			1: "Trouser", 
@@ -18,31 +18,70 @@ from MultinominalLogisticRegression import MultinominalLogisticRegression
 labels = {0: "T-shirt/top",
 			1: "Trouser"}
 
-if __name__ == "__main__":
-	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
-	x_original_validation, y_original_validation = x_original_train[-10000:], y_original_train[-10000:]
-	x_original_train, y_original_train = x_original_train[:-10000], y_original_train[:-10000]
-
-	mlr = MultinominalLogisticRegression()
-
-	coefficients = np.array([])
-	for label in labels:
-		y_train = y_original_train.copy()
-		y_train[y_original_train == label] = 1
-		y_train[y_original_train != label] = 0
-
-		mlr.fit(x_original_train, y_train)
-
-		coefficients = np.append(coefficients, mlr.coefficients)
-	coefficients = coefficients.reshape(len(labels.keys()), x_original_validation.shape[0])
-
+def score(coefficients, x):
 	scores = np.array([])
 	for coef in coefficients:
-		scores.append(np.sum(coef * x_original_validation))
+		print(np.sum(coef * x))
+		scores = np.append(scores, np.sum(coef * x))
 
+	return scores
+
+def softmax(scores):
 	predictions = np.array([])
 	sum_exponential = np.sum(np.exp(scores))
 	for score in scores:
 		predictions.append(np.exp(score) / sum_exponential)
 
-	
+	return predictions
+
+"""
+	Suppose we want to predict if it is l label or not. Then
+	1) Change all labels == l to 1
+	2) Change all labels != l to 0
+	"""
+def toggle_class(x, label):
+	y = x.copy()
+	y[x == label] = 1
+	y[x != label] = 0
+
+	return y
+
+if __name__ == "__main__":
+	# get the training data
+	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
+	x_original_validation, y_original_validation = x_original_train[-10000:], y_original_train[-10000:]
+	x_original_train, y_original_train = x_original_train[:-10000], y_original_train[:-10000]
+
+	# pixels normalized to [0, 1] interval
+	x_original_train = x_original_train / 255
+	x_original_validation = x_original_validation / 255
+
+	# add 1 column to x
+	x_original_train = np.c_[np.ones(x_original_train.shape[0]), x_original_train]
+	x_original_validation = np.c_[np.ones(x_original_validation.shape[0]), x_original_validation]
+
+	# initial values
+	initialGuess = np.ones(x_original_train.shape[1]) # MELHORAR: quais valores de theta comecar?
+	learningRate = 0.01 # MELHORAR: qual valor?
+	iterations = 1000 # MELHORAR: quantas iterações?
+
+	coefficients = np.array([])
+	for label in labels:
+		start_time = time.time()
+
+		mlr = MultinomialLogisticRegression()
+
+		y_train = toggle_class(y_original_train, label)
+
+		mlr.fit(x_original_train, y_train, labels[label], initialGuess, learningRate, iterations)
+
+		y_validation = toggle_class(y_original_validation, label)
+
+		coefficients = np.append(coefficients, mlr.coefficients)
+
+		elapsed_time = time.time() - start_time
+		print("Elapsed time: %1f s" %(elapsed_time))
+
+	scores = score(coefficients, x_original_validation)
+
+	predictions = softmax(scores)

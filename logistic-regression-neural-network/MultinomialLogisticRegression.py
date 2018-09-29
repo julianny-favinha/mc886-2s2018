5c8b97113e3ced1bc0f394beb70a90811307d758
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-29 14:05:51 -0300

    Multinomial. still not working.

--- logistic-regression-neural-network/MultinomialLogisticRegression.py
+++ logistic-regression-neural-network/MultinomialLogisticRegression.py
@@ -8,7 +8,8 @@ class MultinomialLogisticRegression:
 	@staticmethod
 	def softmax(coefficients, xs):
 		score = np.dot(coefficients, xs.T)
-		return np.exp(score) / (np.sum(np.exp(score)))
+		exponent = np.exp(score)
+		return exponent / (np.sum(exponent))
 
 	model = softmax
 
@@ -19,5 +20,9 @@ class MultinomialLogisticRegression:
 		print("Performing fit for {}...".format(label))
 
 		self.coefficients, cost_iterations = descent(initialGuess, self.softmax, x, y, learningRate, iterations, costFunction)
-
 		return cost_iterations
+
+	def predict(self, x, label):
+		print("Performing predictions for {}...".format(label))
+
+		return self.softmax(self.coefficients, x)
--- logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
+++ logistic-regression-neural-network/TrainMultinomialLogisticRegression.py
@@ -17,18 +17,6 @@ labels = {0: "T-shirt/top",
  			8: "Bag",
  			9: "Ankle boot"}
 
-"""
-	Suppose we want to predict if it is l label or not. Then
-	1) Change all labels == l to 1
-	2) Change all labels != l to 0
-	"""
-def toggle_class(x, label):
-	y = x.copy()
-	y[x == label] = 1
-	y[x != label] = 0
-
-	return y
-
 if __name__ == "__main__":
 	# get the training data
 	x_original_train, y_original_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
@@ -45,21 +33,23 @@ if __name__ == "__main__":
 
 	# initial values
 	initialGuess = np.ones((len(labels), x_original_train.shape[1])) # MELHORAR: quais valores de theta comecar?
-	learningRate = 0.00001 # MELHORAR: qual valor?
-	iterations = 1000 # MELHORAR: quantas iterações?
+	learningRate = 0.0001 # MELHORAR: qual valor?
+	iterations = 10 # MELHORAR: quantas iterações?
+
+	print("Using learning rate = {} and {} iterations".format(learningRate, iterations))
 
 	start_time = time.time()
 
 	print("Creating one hot encoding for target...")
-	y_hot_encoding = np.array([])
+	y_hot_encoding = list()
 	for index in y_original_train:
-		y_line = np.zeros(len(labels))
+		y_line =  [0 for _ in range(len(labels))]
 		y_line[index] = 1
-		y_hot_encoding = np.append(y_hot_encoding, y_line)
-	y_hot_encoding = y_hot_encoding.reshape(len(labels), x_original_train.shape[0])
+		y_hot_encoding.append(y_line)
+	y_hot_encoding = np.array(y_hot_encoding)
 	
 	mlr = MultinomialLogisticRegression()
-	cost_iterations = mlr.fit(x_original_train, y_hot_encoding, "Multinomial", initialGuess, learningRate, iterations, cross_entropy)
+	cost_iterations = mlr.fit(x_original_train, y_hot_encoding.T, "Multinomial", initialGuess, learningRate, iterations, cross_entropy)
 
 	plt.plot([x for x in range(iterations)], cost_iterations, color="blue")
 	plt.title("Multinomial Logistic Regression with learningRate = {}".format(learningRate))
@@ -67,6 +57,9 @@ if __name__ == "__main__":
 	plt.xlabel("Number of iterations")
 	plt.ylabel("Cost")
 	plt.clf()
+
+	predictions = mlr.predict(x_original_validation, "Multinomial")
+	print((np.sum(predictions, axis=0)).shape)
 
 	elapsed_time = time.time() - start_time
 	print("Elapsed time: %1f s" %(elapsed_time))

26a3f700729e3768516052c255359a8b1210955e
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-30 18:09:16 -0300

    Receivinf Fashion MNIST data, reLU function, training to 100 iterations

--- logistic-regression-neural-network/NeuralNetworkOne.py
+++ logistic-regression-neural-network/NeuralNetworkOne.py
@@ -1,31 +1,42 @@
 import numpy as np
 
-# def reLU(z):
-# 	return np.maximum(np.zeros(z.shape), z)
+
 
 class NeuralNetwork(object):
 	def __init__(self):
-		self.inputLayerSize = 3
-		self.outputLayerSize = 2
-		self.hiddenLayerSize = 3
+		self.inputLayerSize = 784
+		self.hiddenLayerSize = 784
+		self.outputLayerSize = 10
 
-		self.weights1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-		self.weights2 = np.array([[10, 11], [12, 13], [14, 15]])
+		self.weights1 = np.random.rand(self.inputLayerSize, self.hiddenLayerSize)
+		self.weights2 = np.random.rand(self.hiddenLayerSize, self.outputLayerSize)
 
 	def forward(self, X):
 		self.z2 = np.dot(X, self.weights1)
-		self.a2 = self.(self.z2)
+		self.a2 = self.sigmoid(self.z2)
 		self.z3 = np.dot(self.a2, self.weights2)
 		self.outputLayer = self.sigmoid(self.z3)
 		return self.outputLayer
 
 	def sigmoidPrime(self, z):
+		print("sigmoidPrime")
+		print(np.exp(-z)/((1+np.exp(-z))**2))
 		return np.exp(-z)/((1+np.exp(-z))**2)
 
 	def sigmoid(self, z):
 		return 1 / (1 + np.exp(-z))
 
+	# def reLU(self, z):
+	# 	return np.maximum(np.zeros(z.shape), z)
+
+	# def reLUPrime(self, z):
+	# 	new_z = np.copy(z)
+	# 	new_z[new_z > 0] = 1
+	# 	new_z[new_z <= 0] = 0
+	# 	return new_z
+
 	def lossFunction(self, h, y):
+		print("h", h)
 		sum = np.sum(y*np.log(h + 0.00000001) + (1 - y)*np.log(1 - h + 0.00000001))
 		return -(1/y.shape[0]) * sum
 
--- logistic-regression-neural-network/TrainNeuralNetworkOne.py
+++ logistic-regression-neural-network/TrainNeuralNetworkOne.py
@@ -1,29 +1,66 @@
 import numpy as np
-from matplotlib.pyplot import plot, grid, legend, show
+import mnist_reader
 
-input_layer = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
-y = np.array([[1, 0], [0, 1], [0, 1]])
+from NeuralNetworkOne import NeuralNetwork
+# from matplotlib.pyplot import plot, grid, legend, show
 
-NN = NeuralNetwork()
+labels = {0: "T-shirt/top", 
+ 			1: "Trouser", 
+ 			2: "Pullover", 
+ 			3: "Dress", 
+ 			4: "Coat", 
+ 			5: "Sandal", 
+ 			6: "Shirt", 
+ 			7: "Sneaker", 
+ 			8: "Bag",
+ 			9: "Ankle boot"}
 
-output_layer = NN.forward(input_layer)
-print("output_layer", output_layer)
+def softmax(z):
+	exponent = np.exp(z)
+	return exponent / (np.sum(exponent))
 
-loss = NN.lossFunction(output_layer, y)
-print("loss", loss)
+if __name__ == "__main__":
+	x_layer_train, y_layer_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
+	x_layer_validation, y_layer_validation = x_layer_train[-10000:], y_layer_train[-10000:]
+	x_layer_train, y_layer_train = x_layer_train[:-10000], y_layer_train[:-10000]
 
-dJdW1, dJdW2 = NN.JPrime(input_layer, y) 
-print(dJdW1)
-print(dJdW2)
+	x_layer_train = x_layer_train / 255
+	x_layer_validation = x_layer_validation / 255
 
-learningRate = 1
+	print("Creating one hot encoding for target...")
+	y_hot_encoding = list()
+	for index in y_layer_train:
+		y_line =  [0 for _ in range(len(labels))]
+		y_line[index] = 1
+		y_hot_encoding.append(y_line)
+	y_hot_encoding = np.array(y_hot_encoding)
 
-NN.weights1 -= learningRate * dJdW1
-NN.weights2 -= learningRate * dJdW2
+	NN = NeuralNetwork()
 
-# test = np.arange(-5, 5, 0.1)
-# plot(test, NN.sigmoid(test))
-# plot(test, sigmoidDerivative(test))
-# grid(1)
-# legend(['sigmoid', 'derivative'])
-# show()
+	for _ in range(100):
+		output_layer = NN.forward(x_layer_train)
+		print("output_layer", output_layer)
+
+		output_layer_softmax = softmax(NN.z3)
+		print("softmax", output_layer_softmax)
+
+		loss = NN.lossFunction(output_layer_softmax, y_hot_encoding)
+		print("loss", loss)
+
+		dJdW1, dJdW2 = NN.JPrime(x_layer_train, y_hot_encoding) 
+
+		learningRate = 1
+
+		NN.weights1 -= learningRate * dJdW1
+		NN.weights2 -= learningRate * dJdW2
+		print("dJdW1")
+		print(dJdW1)
+		print("weights1")
+		print(NN.weights1)
+
+	# test = np.arange(-5, 5, 0.1)
+	# plot(test, NN.sigmoid(test))
+	# plot(test, sigmoidDerivative(test))
+	# grid(1)
+	# legend(['sigmoid', 'derivative'])
+	# show()

d5b4de0fe88387de154e14db4f31afd33fd7697f
Julianny Favinha <julianny.favinha@gmail.com>
2018-10-02 20:57:56 -0300

    Mini Batch mode

--- logistic-regression-neural-network/NeuralNetworkOne.py
+++ logistic-regression-neural-network/NeuralNetworkOne.py
@@ -1,30 +1,42 @@
 import numpy as np
 
-
-
 class NeuralNetwork(object):
 	def __init__(self):
-		self.inputLayerSize = 784
-		self.hiddenLayerSize = 784
+		self.inputLayerSize = 785
+		self.hiddenLayerSize = 30
 		self.outputLayerSize = 10
 
 		self.weights1 = np.random.rand(self.inputLayerSize, self.hiddenLayerSize)
 		self.weights2 = np.random.rand(self.hiddenLayerSize, self.outputLayerSize)
+		self.bias2 = np.ones((10, self.outputLayerSize))
 
 	def forward(self, X):
+		# print("X[0]", X[0])
+		# print("weights1[0]", self.weights1[0])
 		self.z2 = np.dot(X, self.weights1)
+		# print("z2[0]", self.z2[0])
 		self.a2 = self.sigmoid(self.z2)
-		self.z3 = np.dot(self.a2, self.weights2)
-		self.outputLayer = self.sigmoid(self.z3)
-		return self.outputLayer
+		# print("a2[0]", self.a2[0])
+		self.z3 = np.dot(self.a2, self.weights2) + self.bias2
+		# print("z3[0]", self.z3[0])
+		self.a3 = self.z3
+		# print("a3[0]", self.a3[0])
+		return self.a3
 
 	def sigmoidPrime(self, z):
-		print("sigmoidPrime")
-		print(np.exp(-z)/((1+np.exp(-z))**2))
-		return np.exp(-z)/((1+np.exp(-z))**2)
+		# print("sigmoidPrime")
+		# print(np.exp(-z)/((1+np.exp(-z))**2))
+		return self.sigmoid(z) * (1 - self.sigmoid(z))
+		# return np.exp(-z)/(((1+np.exp(-z))**2))
 
 	def sigmoid(self, z):
-		return 1 / (1 + np.exp(-z))
+		z = np.clip( z, -1, 1 )
+		z = 1.0 / (1.0 + np.exp(-z))
+		return z
+
+	def softmax(self, z):
+		exponent = np.exp(z - np.max(z))
+		return exponent / np.sum(exponent, axis=0)
 
 	# def reLU(self, z):
 	# 	return np.maximum(np.zeros(z.shape), z)
@@ -36,20 +48,33 @@ class NeuralNetwork(object):
 	# 	return new_z
 
 	# def lossFunction(self, h, y):
-	# 	print("h", h)
+	# 	print("lossFunction h.shape", h.shape)
+	# 	print("lossFunction y.shape", y.shape)
 	# 	sum = np.sum(y*np.log(h + 0.00000001) + (1 - y)*np.log(1 - h + 0.00000001))
 	# 	return -(1/y.shape[0]) * sum
 
+	# # ARRUMAR: QUAL A DERIVADA?
+	# def lossFunctionPrime(self, X, y):
+	# 	print("y shape", y.shape)
+	# 	delta3 = self.outputLayer - y
+	# 	print("delta3.shape", delta3.shape)
+
+	# 	delta2 = np.dot(self.a2 * (1 - self.a2), self.weights2)
+	# 	print("delta2.shape", delta2.shape)
+
+	# 	return delta2, delta3
+
 	def J(self, h, y):
-		return np.sum(0.5 * (y - h)**2)
+		return np.mean(np.sum((y - h)**2, axis=1))
 
 	def JPrime(self, X, y):
-		self.output_layer = self.forward(X)
-		
-		delta3 = np.multiply(-(y - self.output_layer), self.sigmoidPrime(self.z3))
-		dJdW2 = np.dot(self.a2.T, delta3)
+		delta3 = self.a3 - y
+		a2delta3 = np.dot(self.a2.T, delta3)
 
-		delta2 = np.dot(delta3, self.weights2.T) * self.sigmoidPrime(self.z2)
-		dJdW1 = np.dot(X.T, delta2)
+		delta2 = np.multiply(np.dot(delta3, self.weights2.T), self.sigmoidPrime(self.z2))
+		a1delta2 = np.dot(X.T, delta2)
 
-		return dJdW1, dJdW2
+		deltabias2 = self.sigmoidPrime(self.z3)
+		# print("deltabias2.shape", deltabias2.shape)
+
+		return deltabias2, a1delta2, a2delta3
--- logistic-regression-neural-network/TrainNeuralNetworkOne.py
+++ logistic-regression-neural-network/TrainNeuralNetworkOne.py
@@ -2,7 +2,6 @@ import numpy as np
 import mnist_reader
 
 from NeuralNetworkOne import NeuralNetwork
-# from matplotlib.pyplot import plot, grid, legend, show
 
 labels = {0: "T-shirt/top", 
  			1: "Trouser", 
@@ -14,11 +13,6 @@ labels = {0: "T-shirt/top",
  			7: "Sneaker", 
  			8: "Bag",
  			9: "Ankle boot"}
-labels = {0: "T-shirt/top"}
-
-def softmax(z):
-	exponent = np.exp(z)
-	return exponent / (np.sum(exponent))
 
 if __name__ == "__main__":
 	x_layer_train, y_layer_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
@@ -26,44 +20,66 @@ if __name__ == "__main__":
 	x_layer_train, y_layer_train = x_layer_train[:-10000], y_layer_train[:-10000]
 
 	x_layer_train = x_layer_train / 255
+	x_layer_train = np.c_[np.ones(x_layer_train.shape[0]), x_layer_train]
 	x_layer_validation = x_layer_validation / 255
+	x_layer_validation = np.c_[np.ones(x_layer_validation.shape[0]), x_layer_validation]
 
 	print("Creating one hot encoding for target...")
-	# y_hot_encoding = list()
-	# for index in y_layer_train:
-	# 	y_line =  [0 for _ in range(len(labels))]
-	# 	y_line[index] = 1
-	# 	y_hot_encoding.append(y_line)
-	# y_hot_encoding = np.array(y_hot_encoding)
-	y_hot_encoding = np.copy(y_layer_train)
-	y_hot_encoding[y_hot_encoding != 1] = 0
+	y_hot_encoding = list()
+	for index in y_layer_train:
+		y_line = [0 for _ in range(len(labels))]
+		y_line[index] = 1
+		y_hot_encoding.append(y_line)
+	y_hot_encoding = np.array(y_hot_encoding)
+	# print("y_hot_encoding.shape", y_hot_encoding.shape)
 
 	NN = NeuralNetwork()
+	learningRate = 0.1
 
-	for _ in range(100):
-		output_layer = NN.forward(x_layer_train)
-		print("output_layer", output_layer.shape)
+	# print("weights1 before")
+	# print(NN.weights1)
+	# print("weights2 before")
+	# print(NN.weights2)
 
-		print(NN.z3)
-		output_layer_softmax = softmax(NN.z3)
-		print("softmax", output_layer_softmax)
-		print("y_hot_encoding", y_hot_encoding)
+	for i in range(10):
+		print(i)
+		for batch in range(0, 49990, 10):
+			output_layer = NN.forward(x_layer_train[batch:batch+10])
+			# print("output_layer.shape", output_layer.shape)
+			# print("output_layer")
+			# print(output_layer[0])
+			# print(np.sum(output_layer, axis=1).shape)
 
-		loss = NN.lossFunction(output_layer_softmax, y_hot_encoding)
-		print("loss", loss)
+			loss = NN.J(output_layer, y_hot_encoding[batch:batch+10])
+			# print("lossFunction", loss)
 
-		dJdW1, dJdW2 = NN.lossFunctionPrime(x_layer_train, y_hot_encoding)
+			deltabias2, a1delta2, a2delta3 = NN.JPrime(x_layer_train[batch:batch+10], y_hot_encoding[batch:batch+10])
+			# print(a1delta2)
+			NN.weights1 -= 0.1 * learningRate * a1delta2
+			NN.weights2 -= 0.1 * learningRate * a2delta3
+			NN.bias2 -= 0.1 * learningRate * deltabias2
 
-		learningRate = 1
+		preds = NN.softmax(NN.forward(x_layer_validation[-10:]))
+		print("preds")
+		for pred in preds:
+			print(pred)
 
-		NN.weights1 -= learningRate * dJdW1
-		NN.weights2 -= learningRate * dJdW2
-		print("dJdW1")
-		print(dJdW1)
+	print("weights1 after")
+	print(a1delta2)
+	print("weights2 after")
+	print(a2delta3)
+	preds = NN.forward(x_layer_validation[-10:])
+	print("preds")
+	for pred in preds:
+		print(pred)
 
-	# test = np.arange(-5, 5, 0.1)
-	# plot(test, NN.sigmoid(test))
-	# plot(test, sigmoidDerivative(test))
-	# grid(1)
-	# legend(['sigmoid', 'derivative'])
-	# show()
+	y_hot_encoding = list()
+	for index in y_layer_validation:
+		y_line = [0 for _ in range(len(labels))]
+		y_line[index] = 1
+		y_hot_encoding.append(y_line)
+	y_hot_encoding = np.array(y_hot_encoding)
+	real_y = y_hot_encoding[-10:]
+	print("real_y")
+	for pred in real_y:
+		print(pred)

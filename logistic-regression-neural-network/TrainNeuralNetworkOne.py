Fix one hot encoding, applied softmax, changed to use loss function

logistic-regression-neural-network/TrainNeuralNetworkOne.py
@@ -14,6 +14,7 @@ labels = {0: "T-shirt/top",
  			7: "Sneaker", 
  			8: "Bag",
  			9: "Ankle boot"}
+labels = {0: "T-shirt/top"}
 
 def softmax(z):
 	exponent = np.exp(z)
@@ -28,27 +29,30 @@ if __name__ == "__main__":
 	x_layer_validation = x_layer_validation / 255
 
 	print("Creating one hot encoding for target...")
-	y_hot_encoding = list()
-	for index in y_layer_train:
-		y_line =  [0 for _ in range(len(labels))]
-		y_line[index] = 1
-		y_hot_encoding.append(y_line)
-	y_hot_encoding = np.array(y_hot_encoding)
+	# y_hot_encoding = list()
+	# for index in y_layer_train:
+	# 	y_line =  [0 for _ in range(len(labels))]
+	# 	y_line[index] = 1
+	# 	y_hot_encoding.append(y_line)
+	# y_hot_encoding = np.array(y_hot_encoding)
+	y_hot_encoding = np.copy(y_layer_train)
+	y_hot_encoding[y_hot_encoding != 1] = 0
 
 	NN = NeuralNetwork()
 
 	for _ in range(100):
 		output_layer = NN.forward(x_layer_train)
-		print("output_layer", output_layer)
+		print("output_layer", output_layer.shape)
 
+		print(NN.z3)
 		output_layer_softmax = softmax(NN.z3)
-		print("softmax[0]", output_layer_softmax[0])
-		print("y_hot_encoding[0]", y_hot_encoding[0])
+		print("softmax", output_layer_softmax)
+		print("y_hot_encoding", y_hot_encoding)
 
-		loss = NN.J(output_layer_softmax, y_hot_encoding)
+		loss = NN.lossFunction(output_layer_softmax, y_hot_encoding)
 		print("loss", loss)
 
-		dJdW1, dJdW2 = NN.JPrime(x_layer_train, y_hot_encoding)
+		dJdW1, dJdW2 = NN.lossFunctionPrime(x_layer_train, y_hot_encoding)
 
 		learningRate = 1
 
@@ -56,8 +60,6 @@ if __name__ == "__main__":
 		NN.weights2 -= learningRate * dJdW2
 		print("dJdW1")
 		print(dJdW1)
-		print("weights1")
-		print(NN.weights1)
 
 	# test = np.arange(-5, 5, 0.1)
 	# plot(test, NN.sigmoid(test))
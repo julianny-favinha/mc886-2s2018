ccf1324c5a8b8b21209987a31786b431940a2f08
Julianny Favinha <julianny.favinha@gmail.com>
2018-10-05 20:22:22 -0300

    WIP

--- logistic-regression-neural-network/NNOL.py
+++ logistic-regression-neural-network/NNOL.py
@@ -9,8 +9,8 @@ class NNOL(object):
         # self.rglzn_lambda = 1
         self.learning_rate = learning_rate
         """Inicializa parametros"""
-        self.weights1 = np.random.rand(self.inputLayerSize, self.hiddenLayerSize) * 0.001
-        self.weights2 = np.random.rand(self.hiddenLayerSize, self.outputLayerSize) * 0.001
+        self.weights1 = np.random.rand(self.inputLayerSize, self.hiddenLayerSize)
+        self.weights2 = np.random.rand(self.hiddenLayerSize, self.outputLayerSize)
         self.z2 = None
         self.a2 = None
         self.z3 = None
@@ -21,8 +21,10 @@ class NNOL(object):
     def forward(self, x):
         self.z2 = np.dot(x, self.weights1)
         self.a2 = self.sigmoid(self.z2)
+        print("passou a2")
         self.z3 = np.dot(self.a2, self.weights2)
         self.a3 = self.sigmoid(self.z3)
+        print("passou a3")
         return self.a3
 
 
@@ -34,14 +36,13 @@ class NNOL(object):
 
     @staticmethod
     def sigmoid(z):
-        z = 1.0 / (1.0 + np.exp(-z))
-        return z
+        return 1.0 / (1.0 + np.exp(-z))
 
 
-    def iteration(self,x,y):
+    def iteration(self, x, y):
         self.a3 = self.forward(x)
-        self.cost(x,y)
-        self.cost_prime(x,y)
+        self.cost(x, y)
+        self.cost_prime(x, y)
         self.gradient_descent()
         return self.j
 
@@ -51,10 +52,17 @@ class NNOL(object):
 
 
     def cost_prime(self, x, y):
-        delta3 = np.multiply(-(y - self.a3), self.sigmoid_prime(self.z3))
-        self.djdw2 = np.dot(self.a2.T, delta3)# /x.shape[0] + self.rglzn_lambda*self.weights2
-        delta2 = np.dot(delta3, self.weights2.T) * self.sigmoid_prime(self.z2)
-        self.djdw1 = np.dot(x.T, delta2)#/x.shape[0] + self.rglzn_lambda*self.weights1
+        delta2 = y - self.a3
+        self.djdw2 = np.dot(self.a2.T, delta2)
+
+        delta1 = np.dot(delta2, self.weights2.T) * self.sigmoid_prime(self.a2)
+        self.djdw1 = np.dot(x.T, delta1)
+
+        # print("cost_prime")
+        # delta3 = np.multiply(-(y - self.a3), self.sigmoid_prime(self.z3))
+        # self.djdw2 = np.dot(self.a2.T, delta3)# /x.shape[0] + self.rglzn_lambda*self.weights2
+        # delta2 = np.dot(delta3, self.weights2.T) * self.sigmoid_prime(self.z2)
+        # self.djdw1 = np.dot(x.T, delta2)#/x.shape[0] + self.rglzn_lambda*self.weights1
 
 
     def gradient_descent(self):
--- logistic-regression-neural-network/TrainNNOLRealDataset.py
+++ logistic-regression-neural-network/TrainNNOLRealDataset.py
@@ -1,60 +1,84 @@
+import time
 import numpy as np
 import mnist_reader
-import time
+
 from NNOL import NNOL
+from ParseData import prepare_data, segregate_data
+
+
+labels = {0: "T-shirt/top", 
+          1: "Trouser", 
+          2: "Pullover", 
+          3: "Dress", 
+          4: "Coat", 
+          5: "Sandal", 
+          6: "Shirt", 
+          7: "Sneaker", 
+          8: "Bag",
+          9: "Ankle boot"}
 
 
 def one_hot_encode(array, size):
-	print("Creating one hot encoding for target...")
-	if not isinstance(array, (list, tuple, np.ndarray)):
-		array = [array]
-	encoded_array = list()
-	for i in array:
-		line = [0 for _ in range(size)]
-		line[i] = 1
-		encoded_array.append(line)
-	return np.array(encoded_array)
+    if not isinstance(array, (list, tuple, np.ndarray)):
+        array = [array]
+    
+    encoded_array = list()
+    for i in array:
+        line = [0 for _ in range(size)]
+        line[i] = 1
+        encoded_array.append(line)
 
+    return np.array(encoded_array)
+
+
+def main():
+    '''
+    Carrega os dados do treinamento
+    '''
+    # x_layer_train, y_layer_train = mnist_reader.load_mnist('fashion-mnist/data/fashion',
+    #                                                        kind='train')
+    # x_layer_validation, y_layer_validation = x_layer_train[1], y_layer_train[1]
+    # x_layer_train, y_layer_train = x_layer_train[0], y_layer_train[0]
+
+    # x_layer_train = x_layer_train / 255
+    # x_layer_validation = x_layer_validation / 255
+
+    x_train, y_train = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
+    x_train = prepare_data(x_train)
+    x_train, y_train, x_validation, y_validation = segregate_data(x_train, y_train)
+
+    # x_train = x_train[0:10]
+    # y_train = y_train[0:10]
+    # x_validation = x_validation[0:10]
+    # y_validation = y_validation[0:10]
+
+    #precisa estar na forma correta: 1,784
+    # x_train = np.array([x_train])
+
+    '''
+    Encoda os targets
+    '''
+    y_hot_encoding_train = one_hot_encode(y_train, len(labels))
+
+    start_time = time.time()
+    network = NNOL(learning_rate=0.0000001, inputLayerSize=785, hiddenLayerSize=256, outputLayerSize=10)
+
+    epochs = 5
+    for epoch in range(epochs):
+        print("Forwarding for epoch", epoch)
+        network.iteration(x_train, y_hot_encoding_train)
+        cost = network.j
+        print(cost)
+
+    network.forward(x_validation)
+    print("predicted")
+    print(network.a3[0:3])
+    print("real")
+    y_hot_encoding_validation = one_hot_encode(y_validation, len(labels))
+    print(y_hot_encoding_validation[0:3])
+
+    elapsed_time = time.time() - start_time
+    print("Elapsed time: %1f s" % elapsed_time)
 
 if __name__ == "__main__":
-	'''
-	Constante com nomes das classes 
-	'''
-	labels = {0: "T-shirt/top",
-			  1: "Trouser",
-			  2: "Pullover",
-			  3: "Dress",
-			  4: "Coat",
-			  5: "Sandal",
-			  6: "Shirt",
-			  7: "Sneaker",
-			  8: "Bag",
-			  9: "Ankle boot"}
-
-	'''
-	Carrega os dados do treinamento
-	'''
-	x_layer_train, y_layer_train = mnist_reader.load_mnist('fashion-mnist/data/fashion',
-														   kind='train')
-	x_layer_validation, y_layer_validation = x_layer_train[1], y_layer_train[1]
-	x_layer_train, y_layer_train = x_layer_train[0], y_layer_train[0]
-
-	x_layer_train = x_layer_train / 255
-	x_layer_validation = x_layer_validation / 255
-
-	#precisa estar na forma correta: 1,784
-	x_layer_train = np.array([x_layer_train])
-	'''
-	Encoda os targets
-	'''
-	y_hot_encoding = one_hot_encode(y_layer_train, len(labels))
-
-	start_time = time.time()
-	network = NNOL(learning_rate=0.1, inputLayerSize=784, hiddenLayerSize=256, outputLayerSize=10)
-	for i in range(10):
-		network.iteration(x_layer_train, y_hot_encoding)
-		cost = network.j
-		print(cost)
-
-	elapsed_time = time.time() - start_time
-	print("Elapsed time: %1f s" % elapsed_time)
+    main()

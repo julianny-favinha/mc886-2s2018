fa9cebc9e1e397fd1f145979a02c6719c63bc5f4
Julianny Favinha <julianny.favinha@gmail.com>
2018-10-08 14:40:39 -0300

    Removed comments

--- logistic-regression-neural-network/NeuralNetworkOne.py
+++ logistic-regression-neural-network/NeuralNetworkOne.py
@@ -1,20 +1,21 @@
 import numpy as np
 
 class NeuralNetworkOne(object):
-    def __init__(self, learning_rate, activationFunction, activationDerivativeFunction, inputLayerSize=2, hiddenLayerSize=3, outputLayerSize=1):
-        """Atribuicao de hiperparametros"""
+    def __init__(self, learningRate, activationFunction, activationDerivativeFunction, inputLayerSize=2, hiddenLayerSize=3, outputLayerSize=1):
         self.inputLayerSize = inputLayerSize
         self.hiddenLayerSize = hiddenLayerSize
         self.outputLayerSize = outputLayerSize
-        # self.rglzn_lambda = 1
-        self.learning_rate = learning_rate
-        """Inicializa parametros"""
+
+        self.learning_rate = learningRate
+
         self.weights1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
         self.weights2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+
         self.z2 = None
         self.a2 = None
         self.z3 = None
         self.a3 = None
+
         self.activation_function = activationFunction
         self.activation_derivative_function = activationDerivativeFunction
 
@@ -37,24 +38,14 @@ class NeuralNetworkOne(object):
 
 
     def cost(self, x, y):
-        return (np.sum((y - self.a3) ** 2)) / x.shape[0] #/x.shape[0] + (self.rglzn_lambda/2)*((np.sum(self.weights1**2))+(np.sum(self.weights2**2)))
+        return (np.sum((y - self.a3) ** 2)) / x.shape[0]
 
 
     def cost_prime(self, x, y):
-        # delta2 = y - self.a3
-        # self.djdw2 = np.dot(self.a2.T, delta2)
-        #
-        # delta1 = np.dot(delta2, self.weights2.T) * self.sigmoid_prime(self.a2)
-        # self.djdw1 = np.dot(x.T, delta1)
-
-        # print(self.djdw1)
-        # print(self.djdw2)
-
-        # print("cost_prime")
         delta3 = np.multiply(-(y - self.a3), self.activation_derivative_function(self.z3))
-        djdw2 = np.dot(self.a2.T, delta3)# /x.shape[0] + self.rglzn_lambda*self.weights2
+        djdw2 = np.dot(self.a2.T, delta3)
         delta2 = np.dot(delta3, self.weights2.T) * self.activation_derivative_function(self.z2)
-        djdw1 = np.dot(x.T, delta2)#/x.shape[0] + self.rglzn_lambda*self.weights1
+        djdw1 = np.dot(x.T, delta2)
 
         return djdw1, djdw2
 

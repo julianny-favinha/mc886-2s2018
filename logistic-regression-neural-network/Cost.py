79a037b57d6b473f6d3a5fc81da46dd248691f73
Julianny Favinha <julianny.favinha@gmail.com>
2018-10-08 17:34:23 -0300

    bbox to fit title in image

--- logistic-regression-neural-network/Cost.py
+++ logistic-regression-neural-network/Cost.py
@@ -5,5 +5,5 @@ def plot_cost(cost_iterations, label, iterations, learningRate):
     plt.title(f'Logistic Regression for label {label} with learningRate = {learningRate}')
     plt.xlabel('Number of iterations')
     plt.ylabel('Cost')
-    plt.savefig('CostGraph' + label.replace("/", "-"))
+    plt.savefig('CostGraph' + label.replace("/", "-"), bbox_inches="tight")
     plt.clf()
